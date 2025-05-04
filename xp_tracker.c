c1d031aab0d852547510db1eb13f4ddd7e9488b7
SpaceGuy42 <lockema85@gmail.com>
2025-05-04 14:31:59 +0700

    Initial commit: XP Tracker basic loop version

---
+++ xp_tracker.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main() {
+    int xp = 0;                                             //Total XP the user has earned
+    int tasks = 0;                                          //Number of tasks entered by the user
+
+    while (1) {                                             //Infine Loop: runs until broken manually
+        printf("Enter tasks completed (O to exit): ");
+        scanf("%d", &tasks);                                //Take user input and store it in tasks
+
+        if (tasks == 0) {
+            break;                                          //Exit the Loop if user inputs 0
+        }
+
+        xp += tasks * 10;                                   //Add XP based on number of tasks
+        printf("XP Gained: %d | Total XP: %d\n", tasks * 10, xp);
+    }
+
+    printf("Mission complete. Total XP earned: %d\n", xp);
+    return 0;                                               //Exit program
+}
